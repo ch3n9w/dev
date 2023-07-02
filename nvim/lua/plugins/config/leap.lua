9b6a986bac3e0b648a7cb49678b83287b0b5cfd1
ch3n9w <ch3n9w@gmail.com>
2023-07-02 12:13:27 +0800

    update

--- nvim/lua/plugins/config/leap.lua
+++
@@ -1,6 +0,0 @@
-M = function ()
-    require'leap'.add_default_mappings()
-    -- etovxqpdygfblzhcksran
-end
-
-return M
--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-rsync -a -r ~/.config/tmux ./
-rsync -a -r ~/.config/nvim ./
-rsync -a -r ~/.config/alacritty ./
+rsync -av --delete ~/.config/tmux/ ./tmux/
+rsync -av --delete ~/.config/nvim/ ./nvim/
+rsync -av --delete ~/.config/alacritty/ ./alacritty/
 # rsync -a -r ~/.config/zsh ./
 # rsync -a ~/.zshrc ./zshrc
