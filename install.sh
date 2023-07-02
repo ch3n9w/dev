d778116035b23f0febee4ead582f32209541584d
ch3n9w <ch3n9w@gmail.com>
2023-07-02 02:33:27 -0400

    nothing

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
+BASEDIR=~/.config
+rm -r ~/.config/alacritty
+yes | rm -r ~/.config/tmux
+rm -r ~/.config/nvim
+rm -r ~/.config/zsh
+rm -r ~/.zshrc
+
 cp -r ./nvim ~/.config/
 cp -r ./tmux ~/.config/
 cp -r ./alacritty ~/.config/
 cp -r ./zsh ~/.config/
 cp -r ./zshrc ~/.zshrc
 
-# then run tmux and use <prefix> + I to install plugins
