5771f0193444dd9c958af65877968ac2088190dd
ch3n9w <ch3n9w@gmail.com>
2023-07-02 03:05:11 -0400

    use submodule to add plugins

--- install.sh
+++ install.sh
@@ -1,13 +1,34 @@
-BASEDIR=~/.config
-rm -r ~/.config/alacritty
-yes | rm -r ~/.config/tmux
-rm -r ~/.config/nvim
-rm -r ~/.config/zsh
-rm -r ~/.zshrc
+git submodule init zsh/plugins/powerlevel10k
+git submodule init zsh/plugins/zsh-autosuggestions
+git submodule init zsh/plugins/zsh-autoswitch-virtualenv
+git submodule init zsh/plugins/zsh-extract
+git submodule init zsh/plugins/zsh-history-substring-search
+git submodule init zsh/plugins/zsh-syntax-highlighting
+git submodule init tmux/plugins/tpm
+git submodule init tmux/plugins/tmux-sensible
+git submodule init tmux/plugins/tmux-yank
+git submodule init tmux/plugins/tmux-net-speed
+git submodule init tmux/plugins/vim-tmux-navigator
+git submodule update
 
-cp -r ./nvim ~/.config/
+BASEDIR=~/.config
+rm -r $BASEDIR/alacritty
+yes | rm -r $BASEDIR/tmux
+rm -r $BASEDIR/zsh
+rm -r ~/.zshrc
 cp -r ./tmux ~/.config/
 cp -r ./alacritty ~/.config/
 cp -r ./zsh ~/.config/
 cp -r ./zshrc ~/.zshrc
+
+license=$BASEDIR/nvim/intelephense_license_key
+if [ -e "$license" ]; then
+  content=$(cat $license)
+  rm -r $BASEDIR/nvim
+  cp -r ./nvim ~/.config/
+  echo $content > $BASEDIR/nvim/intelephense_license_key
+else
+  rm -r $BASEDIR/nvim
+  cp -r ./nvim ~/.config/
+fi
 
--- update.sh
+++ update.sh
@@ -4,11 +4,3 @@ rsync -av --delete ~/.config/alacritty/ ./alacritty/
 rsync -av --delete ~/.config/zsh/ ./zsh/
 rsync -a ~/.zshrc ./zshrc
 
-# Install zsh plugins
-git clone --depth=1 https://github.com/romkatv/powerlevel10k ./zsh/plugins/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ./zsh/plugins/zsh-autosuggestions
-git clone https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git ./zsh/plugins/zsh-autoswitch-virtualenv
-git clone https://github.com/le0me55i/zsh-extract.git ./zsh/plugins/zsh-extract
-git clone https://github.com/zsh-users/zsh-history-substring-search ./zsh/plugins/zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ./zsh/plugins/zsh-syntax-highlighting
-
