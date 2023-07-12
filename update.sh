4cc9241e24e5102c45f98f214338f7bf957473a8
ch3n9w <ch3n9w@gmail.com>
2023-07-12 20:05:14 +0800

    remove some useless code

--- update.sh
+++ update.sh
@@ -4,17 +4,3 @@ cp -r ~/.config/kitty/ ./
 cp ~/.config/tmux/* ./tmux/
 cp ~/.config/zsh/* ./zsh/
 cp -r ~/.zshrc ./zshrc
-
-# git submodule add --force ssh://git@github.com/romkatv/powerlevel10k.git zsh/plugins/powerlevel10k
-# git submodule add --force ssh://git@github.com/zsh-users/zsh-autosuggestions.git zsh/plugins/zsh-autosuggestions
-# git submodule add --force ssh://git@github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git zsh/plugins/zsh-autoswitch-virtualenv
-# git submodule add --force ssh://git@github.com/le0me55i/zsh-extract.git zsh/plugins/zsh-extract
-# git submodule add --force ssh://git@github.com/zsh-users/zsh-history-substring-search.git zsh/plugins/zsh-history-substring-search
-# git submodule add --force ssh://git@github.com/zsh-users/zsh-syntax-highlighting.git zsh/plugins/zsh-syntax-highlighting
-# git submodule add --force ssh://git@github.com/tmux-plugins/tpm.git tmux/plugins/tpm
-# git submodule add --force ssh://git@github.com/tmux-plugins/tmux-sensible.git tmux/plugins/tmux-sensible
-# git submodule add --force ssh://git@github.com/tmux-plugins/tmux-yank.git tmux/plugins/tmux-yank
-# git submodule add --force ssh://git@github.com/tmux-plugins/tmux-net-speed.git tmux/plugins/tmux-net-speed
-# git submodule add --force ssh://git@github.com/tmux-plugins/tmux-resurrect.git tmux/plugins/tmux-resurrect
-# git submodule add --force ssh://git@github.com/tmux-plugins/tmux-continuum.git tmux/plugins/tmux-continuum
-#
