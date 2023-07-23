e78861f8c3302bb9f76f19c126d8626e81c1e25f
ch3n9w <ch3n9w@gmail.com>
2023-07-23 17:41:18 +0800

    update submodule

--- scripts/submodule.sh
+++ scripts/submodule.sh
@@ -1,9 +1,9 @@
-git submodule add --force ssh://git@github.com/romkatv/powerlevel10k.git zsh/plugins/powerlevel10k
-git submodule add --force ssh://git@github.com/zsh-users/zsh-autosuggestions.git zsh/plugins/zsh-autosuggestions
-git submodule add --force ssh://git@github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git zsh/plugins/zsh-autoswitch-virtualenv
-git submodule add --force ssh://git@github.com/le0me55i/zsh-extract.git zsh/plugins/zsh-extract
-git submodule add --force ssh://git@github.com/zsh-users/zsh-history-substring-search.git zsh/plugins/zsh-history-substring-search
-git submodule add --force ssh://git@github.com/zsh-users/zsh-syntax-highlighting.git zsh/plugins/zsh-syntax-highlighting
+git submodule add --force ssh://git@github.com/romkatv/powerlevel10k.git zsh/zsh/plugins/powerlevel10k
+git submodule add --force ssh://git@github.com/zsh-users/zsh-autosuggestions.git zsh/zsh/plugins/zsh-autosuggestions
+git submodule add --force ssh://git@github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git zsh/zsh/plugins/zsh-autoswitch-virtualenv
+git submodule add --force ssh://git@github.com/le0me55i/zsh-extract.git zsh/zsh/plugins/zsh-extract
+git submodule add --force ssh://git@github.com/zsh-users/zsh-history-substring-search.git zsh/zsh/plugins/zsh-history-substring-search
+git submodule add --force ssh://git@github.com/zsh-users/zsh-syntax-highlighting.git zsh/zsh/plugins/zsh-syntax-highlighting
 git submodule add --force ssh://git@github.com/tmux-plugins/tpm.git tmux/plugins/tpm
 git submodule add --force ssh://git@github.com/tmux-plugins/tmux-sensible.git tmux/plugins/tmux-sensible
 git submodule add --force ssh://git@github.com/tmux-plugins/tmux-yank.git tmux/plugins/tmux-yank
