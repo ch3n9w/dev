e8be2745f300a5387427a44dab8f21ea3baf7b7a
ch3n9w <ch3n9w@gmail.com>
2023-08-22 14:53:50 +0800

    select pylint based on venv

--- nvim/lua/plugins/config/nullls.lua
+++ nvim/lua/plugins/config/nullls.lua
@@ -1,6 +1,5 @@
 M = function()
     local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     null_ls.setup({
         sources = {
             null_ls.builtins.formatting.clang_format.with({
@@ -11,8 +10,16 @@ M = function()
             }),
             null_ls.builtins.formatting.prettier,
             null_ls.builtins.formatting.black,
-            -- dont install pylint with mason, install in virtualenv with pip
-            null_ls.builtins.diagnostics.pylint,
+            null_ls.builtins.diagnostics.pylint.with({
+                command = function ()
+                    local cwd = vim.fn.getcwd()
+                    if vim.fn.executable(cwd .. '/.venv/bin/pylint') == 1 then
+                        return cwd .. '/.venv/bin/pylint'
+                    else
+                        return 'pylint'
+                    end
+                end
+            }),
             null_ls.builtins.formatting.isort,
         },
     })
