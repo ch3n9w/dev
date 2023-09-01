4ddc48ff108cbb605102212f0d82e96c333a0309
ch3n9w <ch4xer@gmail.com>
2023-09-01 19:56:00 +0800

    adjust keymap for zsh,remove some function

--- zsh/zsh/functions.zsh
+++ zsh/zsh/functions.zsh
@@ -63,22 +63,6 @@ function fzf_rg () {
     rg -l "${selected_pattern}" | fzf 
 }
 
-function tag () {
-    # tag should be like xxx.tag
-    local base_dir="/home/ch4ser/Documents/Paper"
-    local current_dir=$(pwd)
-    if [[ "$current_dir" != "$base_dir"* ]] then
-        # local result=$(rg -l "${selected_pattern}" | fzf --delimiter / --with-nth -2)
-        # local result=$(fd -i "tags/${selected_pattern}" $directory | fzf --delimiter / --with-nth -3 --no-preview)
-        # local result=$(fd --follow -t f | grep 'tags' | fzf --no-preview)
-        builtin cd $base_dir
-    fi
-    local result=$(fd --follow -t f -e tag | fzf --no-preview)
-
-    local paper=$(dirname $result)
-    builtin cd $paper
-    redraw-prompt
-}
 
 filemanager() {
     # avoid nested ranger instances
@@ -99,28 +83,12 @@ man() {
     env MANWIDTH=$width \
     man "$@"
 }
+
 # make alacritty show current directory in title
 precmd() {
     [[ $TERM == "xterm-256color" ]] && print "\033]0;:> $(pwd)"
 }
 
-code()
-{
-    if [ -d "${1}" ]; then
-        local dir="${1}"
-        shift 1
-        # ( cd "${dir}" && NVIM_GUI="2" kitty --class 'neovide' -e 'nvim' . &)
-        ( cd "${dir}" && kitty --config ~/.config/kitty/code.conf --class 'neovide' -e 'nvim' . &)
-    else
-        nvim "${@}"
-    fi
-}
 
-
-zle -N cd_fzf
-zle -N cd_fzf_from_home
-zle -N open_fzf
-zle -N fzf_rg
-zle -N tag
 zle -N filemanager
 
--- zsh/zsh/keybinds.zsh
+++ zsh/zsh/keybinds.zsh
@@ -1,15 +1,7 @@
 
 # keybindings must be provided after plugin source
-bindkey -e
-# set title
-bindkey '\er' filemanager
-bindkey '\ef' cd_fzf
-bindkey '\eF' cd_fzf_from_home
-bindkey '\eo' open_fzf
+bindkey '^R' filemanager
 bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
-
-# in tmux, I can only use alt-backspace
-bindkey '^H' backward-kill-word
