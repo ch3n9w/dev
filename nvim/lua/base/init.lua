91bd47f12a22246dbdbf0e948de7ee63ded445ef
ch3n9w <ch4xer@gmail.com>
2023-09-01 10:12:40 +0800

    remove cursorcolumn

--- nvim/lua/base/init.lua
+++ nvim/lua/base/init.lua
@@ -21,7 +21,6 @@ vim.opt.updatetime = 300
 vim.opt.smartcase = true
 vim.opt.ignorecase = true
 vim.opt.cursorline = true
-vim.opt.cursorcolumn = true
 
 -- clipboard
 vim.opt.clipboard = 'unnamedplus'
