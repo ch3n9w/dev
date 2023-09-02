08c688d18d408e6d60ebe93cf6ee6332cd857039
ch3n9w <ch4xer@gmail.com>
2023-09-02 15:20:08 +0800

    add reverse menu selection for zsh

--- zsh/zsh/keybinds.zsh
+++ zsh/zsh/keybinds.zsh
@@ -5,3 +5,4 @@ bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
+bindkey '^[[Z' reverse-menu-complete
