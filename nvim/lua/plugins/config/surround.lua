8b731239d0154a9472d173035e7dea6131bbce8f
ch3n9w <ch3n9w@gmail.com>
2023-09-05 21:35:09 +0800

    update surround

--- nvim/lua/plugins/config/surround.lua
+++ nvim/lua/plugins/config/surround.lua
@@ -1,10 +1,33 @@
 M = function()
-    require("nvim-surround").setup{
+    require("nvim-surround").setup {
         keymaps = {
             visual = "s",
             delete = "ds",
             change = "cs",
         },
+        surrounds = {
+            ["("] = {
+                add = { "(", ")" },
+                find = function()
+                    return M.get_selection({ motion = "a(" })
+                end,
+                delete = "^(.?)().-(?.)()$",
+            },
+            ["{"] = {
+                add = { "{", "}" },
+                find = function()
+                    return M.get_selection({ motion = "a{" })
+                end,
+                delete = "^(.?)().-(?.)()$",
+            },
+            ["["] = {
+                add = { "[", "]" },
+                find = function()
+                    return M.get_selection({ motion = "a[" })
+                end,
+                delete = "^(.?)().-(?.)()$",
+            },
+        }
     }
 end
 
