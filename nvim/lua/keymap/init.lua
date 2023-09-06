8d169156dca21aa56fbb119474f8fc2a4529711e
ch3n9w <ch3n9w@gmail.com>
2023-09-06 23:46:13 +0800

    update keymap

--- nvim/lua/keymap/init.lua
+++ nvim/lua/keymap/init.lua
@@ -1,69 +1,63 @@
 local vim = vim
 local custom = require('keymap.custom')
-local Movement = {
-    -- move cursor in wrapline paragraph
-    { 'n',          'j',          "v:count == 0 ? 'gj' : 'j'",                    { expr = true, silent = true } },
-    { 'n',          'k',          "v:count == 0 ? 'gk' : 'k'",                    { expr = true, silent = true } },
-    -- move cursor to the start and the end of paragraph
-    { 'i',          '<C-j>',      '<ESC>o', },
-    { 'i',          '<C-k>',      '<ESC>O', },
-    { 'i',          '<C-h>',      '<ESC>I' },
-    { 'i',          '<C-l>',      '<ESC>A' },
-    { 'n',          '(',          '<ESC>^' },
-    { 'n',          ')',          '<ESC>$' },
-    { { 'n', 'v' }, 'L',          '$' },
-    { { 'n', 'v' }, 'H',          '^' },
+local Base = {
+    movement = {
+        -- move cursor in wrapline paragraph
+        { 'n',          'j',          "v:count == 0 ? 'gj' : 'j'",                    { expr = true, silent = true } },
+        { 'n',          'k',          "v:count == 0 ? 'gk' : 'k'",                    { expr = true, silent = true } },
+        { { 'n', 'v' }, 'L',          '$' },
+        { { 'n', 'v' }, 'H',          '^' },
 
-    -- tab switch, dont modify <Tab>, which will affect <C-i>
-    { 'n',          'K',          '<CMD>bnext!<CR>' },
-    { 'n',          'J',          '<CMD>bprevious!<CR>' },
+        -- tab switch, dont modify <Tab>, which will affect <C-i>
+        { 'n',          'K',          '<CMD>bnext!<CR>' },
+        { 'n',          'J',          '<CMD>bprevious!<CR>' },
 
-    -- move window
-    { 'n',          '<C-S-h>',    '<C-w>H' },
-    { 'n',          '<C-S-j>',    '<C-w>J' },
-    { 'n',          '<C-S-k>',    '<C-w>K' },
-    { 'n',          '<C-S-l>',    '<C-w>L' },
+        -- move window
+        { 'n',          '<C-S-h>',    '<C-w>H' },
+        { 'n',          '<C-S-j>',    '<C-w>J' },
+        { 'n',          '<C-S-k>',    '<C-w>K' },
+        { 'n',          '<C-S-l>',    '<C-w>L' },
 
-    -- page scroll
-    { { 'n', 'v' }, '<PageUp>',   math.floor(vim.fn.winheight(0) / 2) .. '<C-u>', { desc = 'scroll 1/3 size of page' } },
-    { { 'n', 'v' }, '<PageDown>', math.floor(vim.fn.winheight(0) / 2) .. '<C-d>', { desc = 'scroll 1/3 size of page' } },
+        -- page scroll
+        { { 'n', 'v' }, '<PageUp>',   math.floor(vim.fn.winheight(0) / 2) .. '<C-u>',
+                                                                                          { desc =
+            'scroll 1/2 size of page' } },
+        { { 'n', 'v' }, '<PageDown>', math.floor(vim.fn.winheight(0) / 2) .. '<C-d>',
+                                                                                          { desc =
+            'scroll 1/2 size of page' } },
+    },
+    edit = {
+        { 'i', '<C-BS>',      '<C-W>', { desc = 'delete word forward' } },
+        { 'v', '<C-c>',       '"*ygvy' },
+        { 'v', 'y',           '"*ygvy' },
+        { 'i', '<C-v>',       '<C-R>+' },
+        { 'v', '>',           '>gv',   { desc = 'keep virtual mode after indent' } },
+        { 'v', '<',           '<gv',   { desc = 'keep virtual mode after indent' } },
+        { 'n', '<Backspace>', 'ciw' },
+        { 'v', '<Backspace>', 'c' },
+    },
+    cmd = {
+        { { 'n', 'v' }, ';',         ':',                 { nowait = true } },
+        { 'n',          '<leader>q', 'q1',                { desc = 'record macro' } },
+        { 'n',          '<C-q>',     ':q<CR>',            { desc = 'quit window' } },
+        { 'n',          'Q',         custom.WriteQuitAll, { desc = 'quit all' } },
+        { 'n',          'g=',        vim.lsp.buf.format },
+        { 'c',          'w!',        custom.sudo_write,   { desc = 'save file as root' } },
+    },
+    fold = {
+        { 'n', '<CR>',          'za', { desc = 'toggle fold' } },
+        { 'n', '<2-LeftMouse>', 'za', { desc = 'toggle fold' } },
+    },
+    modeSwitch = {
+        { 'n', '<RightMouse>', 'a' },
+        { 'i', '<ESC>', '<C-O>:stopinsert<CR>', {
+            desc =
+            'back to normal mode without moving cursor'
+        } },
+    }
 }
 
-local Edit = {
-    { 'i', '<C-BS>',      '<C-W>', { desc = 'delete word forward' } },
-    { 'v', '<C-c>',       '"*ygvy' },
-    { 'v', 'y',           '"*ygvy' },
-    { 'i', '<C-v>',       '<C-R>+' },
-    { 'v', '>',           '>gv',   { desc = 'keep virtual mode after indent' } },
-    { 'v', '<',           '<gv',   { desc = 'keep virtual mode after indent' } },
-    { 'n', '<Backspace>', 'ciw' },
-    { 'v', '<Backspace>', 'c' },
-}
-
-local Cmd = {
-    { 'n', ';',         ':',                 { nowait = true } },
-    { 'v', ';',         ':',                 { nowait = true } },
-    { 'n', '<leader>q', 'q1',                { desc = 'record macro' } },
-    { 'n', '<C-q>',     ':q<CR>',            { desc = 'quit window' } },
-    { 'n', 'Q',         custom.WriteQuitAll, { desc = 'quit all' } },
-    { 'n', 'g=',        vim.lsp.buf.format },
-    { 'c', 'w!',        custom.sudo_write,   { desc = 'save file as root' } },
-}
-
-local Fold = {
-    { 'n', '<CR>',          'za', { desc = 'toggle fold' } },
-    { 'n', '<2-LeftMouse>', 'za' },
-}
-
-local ModeSwitch = {
-    { 'n', '<RightMouse>', 'a' },
-    { 'i', '<ESC>', '<C-O>:stopinsert<CR>', {
-        desc =
-        'back to normal mode without moving cursor'
-    } },
-}
-
-local Plugins = {
+local Plugin = {
     bufdelete = {
         { 'n', 'q', custom.DeleteWinOrBuf },
     },
@@ -128,33 +122,24 @@ local Plugins = {
     tabout = {},
 }
 
-local Nop = {
-    { { 'n', 'x' }, 's', 'Nop', { noremap = true, silent = true } },
-}
-
-local key_mapper = function(mode, key, result, config)
-    if nil == config then
-        config = { noremap = true, silent = true }
-    end
-    vim.keymap.set(
-        mode,
-        key,
-        result,
-        config
-    )
-end
-
-for _, keymap_class in ipairs({ Movement, Edit, Cmd, Fold, ModeSwitch, Nop }) do
-    for _, keymap in ipairs(keymap_class) do
-        key_mapper(keymap[1], keymap[2], keymap[3], keymap[4])
+local keyMapper = function(keySet)
+    for _, set in pairs(keySet) do
+        for _, keymap in ipairs(set) do
+            if keymap[4] == nil then
+                keymap[4] = { noremap = true, silent = true }
+            end
+            vim.keymap.set(
+                keymap[1],
+                keymap[2],
+                keymap[3],
+                keymap[4]
+            )
+        end
     end
 end
 
-for _, plugin_keymap in pairs(Plugins) do
-    for _, keymap in ipairs(plugin_keymap) do
-        key_mapper(keymap[1], keymap[2], keymap[3], keymap[4])
-    end
-end
+keyMapper(Base)
+keyMapper(Plugin)
 
 
 vim.api.nvim_create_user_command(
