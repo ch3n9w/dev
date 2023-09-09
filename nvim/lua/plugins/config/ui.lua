19e873720137a11f94b79075067dfaf32f7c7ddd
ch3n9w <ch3n9w@gmail.com>
2023-09-09 21:38:02 +0800

    remove redundant code in ui.lua

--- nvim/lua/plugins/config/ui.lua
+++ nvim/lua/plugins/config/ui.lua
@@ -1,48 +1,20 @@
 M = function()
-    local trans
-    local dims
-    local theme
-    local tree_theme
-    -- if start nvim-qt with NVIM_GUI, then the background will not be transparent
-    if os.getenv("NVIM_GUI") == "1" then
-        trans = false
-        dims = false
-        tree_theme = "dark"
-        theme = 'night'
-    elseif os.getenv("NVIM_GUI") == "2" then
-        trans = false
-        dims = false
-        tree_theme = "dark"
-        theme = 'storm'
-    elseif os.getenv("NVIM_GUI") == "3" then
-        trans = false
-        dims = false
-        tree_theme = "light"
-        theme = "day"
-    else
-        trans = true
-        dims = true
-        tree_theme = "transparent"
-        theme = 'storm'
-    end
-
-
     require("tokyonight").setup({
-        style = theme,
-        transparent = trans,
+        style = "storm",
+        transparent = true,
         terminal_colors = true,
         styles = {
             comments = { italic = true },
             keywords = { italic = true },
             functions = {},
             variables = {},
-            sidebars = tree_theme,
+            sidebars = "transparent",
             floats = "dark",
         },
         sidebars = { "qf", "help" },
         day_brightness = 0.1,
         hide_inactive_statusline = false,
-        dim_inactive = dims,
+        dim_inactive = true,
         lualine_bold = true,
 
         on_colors = function(colors)
