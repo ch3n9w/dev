e34b9199822839102e0cd16d8b8e83ab10f06a86
ch3n9w <ch3n9w@gmail.com>
2023-09-09 22:15:29 +0800

    update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,13 @@
 rm -r nvim
+rm -r zsh
+rm -r alacritty
+rm -r ranger
+rm -r tmux
 cp -r ~/.config/nvim/ ./
 cp -r ~/.config/alacritty/ ./
 cp -r ~/.config/ranger/ ./
-cp ~/.config/tmux/* ./tmux/
-cp ~/.config/zsh/* ./zsh/zsh/
+cp -r ~/.config/tmux/ .
+mkdir zsh
+cp -r ~/.config/zsh/ ./zsh/
 cp -r ~/.zshrc ./zsh/zshrc
 cp -r ~/.config/fontconfig/ ./
