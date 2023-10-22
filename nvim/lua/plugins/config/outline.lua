c1b12cae6aa489e0be75c4be2fd49845f3722627
ch3n9w <ch4xer@gmail.com>
2023-10-22 23:26:58 +0800

    update

--- nvim/lua/plugins/config/outline.lua
+++ nvim/lua/plugins/config/outline.lua
@@ -2,29 +2,13 @@ M = function()
     require("aerial").setup({
         layout = {
             max_width = { 30, 0.2 },
+        },
+        keymaps = {
+            ["<2-LeftMouse>"] = "actions.jump",
+            ["<CR>"] = "actions.jump",
+            ["q"] = "actions.close",
+            ["<BS>"] = "actions.tree_toggle",
         }
     })
-    -- require("symbols-outline").setup({
-    --     highlight_hovered_item = true,
-    --     show_symbol_details = false,
-    --     show_numbers = false,
-    --     auto_close = false,
-    --     width = 15,
-    --     keymaps = {
-    --         close = { "<Esc>", "q" },
-    --         goto_location = "<Cr>",
-    --         focus_location = "o",
-    --         hover_symbol = "<C-space>",
-    --         toggle_preview = "K",
-    --         rename_symbol = "r",
-    --         code_actions = "a",
-    --         fold = "h",
-    --         unfold = "l",
-    --         fold_all = "W",
-    --         unfold_all = "E",
-    --         fold_reset = "R",
-    --     }
-    -- }
-    -- )
 end
 return M
