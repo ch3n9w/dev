44769e9bd3c4a08bfde52fa39114d6376a41d64c
ch3n9w <ch4xer@gmail.com>
2023-10-23 12:31:46 +0800

    change attach_mode of aerial.nvim to global

--- nvim/lua/plugins/config/outline.lua
+++ nvim/lua/plugins/config/outline.lua
@@ -2,7 +2,9 @@ M = function()
     require("aerial").setup({
         layout = {
             max_width = { 30, 0.2 },
+            placement = "edge",
         },
+        attach_mode = "global",
         keymaps = {
             ["<2-LeftMouse>"] = "actions.jump",
             ["<CR>"] = "actions.jump",
