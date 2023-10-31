b092cf9f21cdf2124c1a79ccd8c990aabedeafcb
ch3n9w <ch4xer@gmail.com>
2023-10-31 21:55:35 +0800

    prevent telescope from opening file in insert mode

--- nvim/lua/autocommand/init.lua
+++ nvim/lua/autocommand/init.lua
@@ -99,3 +99,12 @@ vim.api.nvim_create_autocmd({ "CursorHold" }, {
         require("nabla").popup()
     end
 })
+
+-- prevent telescope from opening in insert mode
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})
