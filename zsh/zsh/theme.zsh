ff28e17bb8b379da1f032b15be30a39a14b27e81
ch3n9w <ch4xer@gmail.com>
2023-12-22 16:02:10 +0800

    make directory font not bold

--- zsh/zsh/theme.zsh
+++ zsh/zsh/theme.zsh
@@ -236,7 +236,7 @@
   # segment is always an anchor.
   typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=39
   # Display anchor directory segments in bold.
-  typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=true
+  typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=false
   # Don't shorten directories that contain any of these files. They are anchors.
   local anchor_files=(
     .bzr
