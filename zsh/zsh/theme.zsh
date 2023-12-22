3ab699f59ccaa5d217f6d429114522d9561bf41d
ch3n9w <ch4xer@gmail.com>
2023-12-22 16:29:42 +0800

    change os_icon color

--- zsh/zsh/theme.zsh
+++ zsh/zsh/theme.zsh
@@ -195,7 +195,7 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=255
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=14
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='🐱'
 
