4aedfb64dcaf76ce1aeb7d983d154ee903030340
ch3n9w <ch4xer@gmail.com>
2024-01-16 13:45:53 +0800

    remove submodule

--- scripts/submodule.sh
+++ scripts/submodule.sh
@@ -9,6 +9,5 @@ git submodule add --force https://github.com/tmux-plugins/tpm.git tmux/plugins/t
 git submodule add --force https://github.com/tmux-plugins/tmux-sensible.git tmux/plugins/tmux-sensible
 git submodule add --force https://github.com/tmux-plugins/tmux-resurrect.git tmux/plugins/tmux-resurrect
 git submodule add --force https://github.com/tmux-plugins/tmux-continuum.git tmux/plugins/tmux-continuum
-git submodule add --force https://github.com/olimorris/tmux-pomodoro-plus.git tmux/plugins/tmux-pomodoro-plus
 git submodule add --force https://github.com/alexanderjeurissen/ranger_devicons.git ranger/plugins/ranger_devicons
 git submodule add --force https://github.com/SL-RU/ranger_udisk_menu.git ranger/plugins/ranger_udisk_menu
