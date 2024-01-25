8d9192b5c313622d9e0819aebf7bdfcc287b56a3
ch3n9w <ch3n9w@gmail.com>
2024-01-25 23:38:26 +0800

    custom Diagnostic sign

--- nvim/lua/base/init.lua
+++ nvim/lua/base/init.lua
@@ -71,5 +71,9 @@ vim.opt.viewoptions = 'folds,cursor,curdir'
 
 vim.g.python3_host_prog = vim.fn.exepath("python3")
 
+vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
 vim.api.nvim_set_hl(0, "Comment", { bold = true, italic = true })
 vim.api.nvim_set_hl(0, "TSComment", { bold = true, italic = true })
