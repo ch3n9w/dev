9f4935388b2c190874455d0479c0c4d9a027868d
ch3n9w <ch3n9w@gmail.com>
2024-01-29 22:00:33 +0800

    remove filetype for markdown-preview.nvim

--- nvim/lua/plugins/note.lua
+++ nvim/lua/plugins/note.lua
@@ -2,7 +2,6 @@ return {
     {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
         build = function() vim.fn["mkdp#util#install"]() end,
         config = function()
             vim.g.mkdp_browser = 'min'
