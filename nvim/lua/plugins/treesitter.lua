61489eecee9c31b6e27042cd283012835d317af7
ch3n9w <ch3n9w@gmail.com>
2024-02-04 11:52:20 +0800

    disable treesitter for latex

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,7 @@ return {
         event = 'BufRead',
         config = function()
             require('nvim-treesitter.configs').setup {
+                -- dont install latex, it's too slow
                 ensure_installed = {
                     "c",
                     "lua",
@@ -26,13 +27,16 @@ return {
                     "xml",
                     "markdown",
                     "markdown_inline",
-                    "latex",
                 },
                 -- switch to buildin syntax highlighting
                 highlight = {
                     enable = true,
-                    disable = function(lang, bufnr)
-                        return lang == "" or vim.api.nvim_buf_line_count(bufnr) > 2000
+                    disable = function(lang, buf)
+                        local max_filesize = 100 * 1024 -- 100 KB
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
                     end,
                     -- additional_vim_regex_highlighting = {"yaml"},
                 },
