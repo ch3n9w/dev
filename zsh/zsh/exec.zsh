25bee5f39c405f40136ff9ab57d8947cab769b21
ch3n9w <ch3n9w@gmail.com>
2024-02-04 22:49:35 +0800

    remove some alias and fzf functions in zsh

--- zsh/zsh/exec.zsh
+++ zsh/zsh/exec.zsh
@@ -1,4 +1,3 @@
 # https://github.com/ajeetdsouza/zoxide/issues/403
 eval "$(zoxide init zsh --cmd cd)"
 source <(kubectl completion zsh)
-# todo.sh list
--- zsh/zsh/functions.zsh
+++ zsh/zsh/functions.zsh
@@ -6,63 +6,6 @@ redraw-prompt() {
   p10k display -r
 }
 
-cd_fzf() {
-    # local fzf_result=$(fzf)
-    local fzf_result=$(fd . --hidden --exclude={.wine,.git,.idea,.vscode,.sass-cache,node_modules,build,.steam,.m2,.rangerdir,.ghidra,.mozilla,.cache} | fzf)
-    if [ -z "$fzf_result" ]
-    then
-        echo "No Directory specific"
-    else
-        if [ -d "$fzf_result" ]; then
-            cd $fzf_result
-        else
-            local directory=$(dirname $fzf_result)
-            cd $fzf_result
-        fi
-    fi
-    redraw-prompt
-}
-
-cd_fzf_from_home() {
-    # local fzf_result=$(fzf)
-    local fzf_result=$(fd . /home/ch4ser --hidden --exclude={.wine,.git,.idea,.vscode,.sass-cache,node_modules,build,.steam,.m2,.rangerdir,.ghidra,.mozilla,.cache} | fzf)
-    if [ -z "$fzf_result" ]
-    then
-        echo "No Directory specific"
-    else
-        if [ -d "$fzf_result" ]; then
-            cd $fzf_result
-        else
-            local directory=$(dirname $fzf_result)
-            cd $fzf_result
-        fi
-    fi
-    redraw-prompt
-}
-
-open_fzf() {
-    local fzf_result=$(fd . $1 --type f --hidden --exclude={.wine,.git,.idea,.vscode,.sass-cache,node_modules,build,.steam,.m2,.rangerdir,.ghidra,.mozilla,.cache} | fzf)
-    local result_type=$(file -0 "$fzf_result" | cut -d $'\0' -f2)
-    if [ -z "$fzf_result" ]
-    then
-        echo "No file specific"
-    else
-        case "$result_type" in
-            (*text*)
-                nvim $fzf_result
-                ;;
-            (*)
-                xdg-open $fzf_result &
-                ;;
-        esac
-    fi
-    redraw-prompt
-}
-function fzf_rg () {
-    local selected_pattern=$(printf '%s' "$@" | sed -e 's/"/\\"/g' -e "s/'/\\\\'/g" -e 's/\[/\\\[/g' -e 's/\]/\\\]/g' -e 's/-/\\-/g')
-    rg -l "${selected_pattern}" | fzf 
-}
-
 filemanager() {
     tmp="$(mktemp)"
     /usr/bin/lf --last-dir-path="$tmp" "$@"
