1662f9dcf80c585c773f0b2da5be239be9e54ff4
ch3n9w <ch4xer@gmail.com>
2024-02-06 12:09:28 +0800

    change neotree auto behavior: auto close

--- nvim/lua/autocommand/init.lua
+++ nvim/lua/autocommand/init.lua
@@ -12,10 +12,9 @@ vim.api.nvim_create_autocmd({ 'InsertLeave', 'BufCreate', 'BufEnter', 'BufLeave'
     command = 'silent !fcitx5-remote -c'
 })
 
-vim.g.width_open_tree = 120
-vim.g.bufenter_check = false
+vim.g.width_close_tree = 120
 
-local toggle_tree = function()
+local close_tree = function()
     local get_filetree_window = function()
         for _, windowId in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
             local buffer = vim.api.nvim_win_get_buf(windowId)
@@ -25,48 +24,19 @@ local toggle_tree = function()
         end
         return nil
     end
-    if vim.api.nvim_list_uis()[1].width > vim.g.width_open_tree then
-        local is_valid_window = function(winnr)
-            local bufnr = vim.api.nvim_win_get_buf(winnr)
-            if not bufnr or bufnr < 1 then return false end
-            local exists = vim.api.nvim_buf_is_valid(bufnr)
-            return vim.bo[bufnr].buflisted and exists
-        end
-        if #vim.tbl_filter(is_valid_window, vim.api.nvim_list_wins()) > 1 then
-            return
-        end
-        if get_filetree_window() ~= nil then
-            return
-        end
-        local open_tree_without_focus = function()
-            vim.cmd("Neotree show last")
-            vim.cmd.wincmd("=")
-        end
-        vim.fn.timer_start(1, open_tree_without_focus)
-    else
+    if vim.api.nvim_list_uis()[1].width <= vim.g.width_close_tree then
         if get_filetree_window() ~= nil then
             vim.cmd("Neotree action=close")
         end
     end
 end
 
-vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-    pattern = { "*.*" },
-    callback = function()
-        -- only execute at the first time entering buffer
-        if vim.g.bufenter_check == false then
-            toggle_tree()
-            vim.g.bufenter_check = true
-        end
-    end
-})
-
 vim.api.nvim_create_autocmd({ 'VimResized' }, {
     pattern = { "*.*" },
     callback = function()
         -- keep the size of every window, very usful!
         vim.cmd.wincmd('=')
-        toggle_tree()
+        close_tree()
     end
 })
 
