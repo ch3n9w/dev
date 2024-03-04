cdfde3f0a90df9f28a89d72ed8f6ae9b04c9251c
ch3n9w <ch4xer@gmail.com>
2024-03-04 11:12:02 +0800

    add event and filetype for markdown-preview.nvim

--- nvim/lua/plugins/note.lua
+++ nvim/lua/plugins/note.lua
@@ -1,7 +1,8 @@
 return {
     {
         "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        event = "VeryLazy",
+        ft = "markdown",
         build = function() vim.fn["mkdp#util#install"]() end,
         config = function()
             vim.g.mkdp_browser = 'min'
