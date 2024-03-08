65ac2a46c378477fcc2e0f065130c15fa1977f65
ch3n9w <ch4xer@gmail.com>
2024-03-08 14:26:31 +0800

    make tabstop to 2 for markdown

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,8 +1,8 @@
 local vim = vim
 vim.opt_local.expandtab = true
-vim.opt_local.shiftwidth = 4
-vim.opt_local.tabstop = 4
-vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
 vim.opt_local.foldmethod = "expr"
 vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
 
