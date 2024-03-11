41d047424262dc31975d655a3fd91968dfe3153f
ch3n9w <ch4xer@gmail.com>
2024-03-11 20:45:45 +0800

    add tmux-cpu module

--- scripts/submodule.sh
+++ scripts/submodule.sh
@@ -9,3 +9,4 @@ git submodule add --force https://github.com/tmux-plugins/tpm.git tmux/plugins/t
 git submodule add --force https://github.com/tmux-plugins/tmux-sensible.git tmux/plugins/tmux-sensible
 git submodule add --force https://github.com/tmux-plugins/tmux-resurrect.git tmux/plugins/tmux-resurrect
 git submodule add --force https://github.com/tmux-plugins/tmux-continuum.git tmux/plugins/tmux-continuum
+git submodule add --force https://github.com/tmux-plugins/tmux-cpu.git tmux/plugins/tmux-cpu
