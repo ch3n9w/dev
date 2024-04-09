dd8b3f5d13a6e2db3cc6e0ad2cedf478ebf9c11b
ch3n9w <ch4xer@gmail.com>
2024-04-09 14:20:50 +0800

    change vim.opt to vim.o

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -1,80 +1,81 @@
 local vim = vim
 
 -- disable statusline, set splitline for windows
-vim.opt.laststatus = 0
+vim.o.laststatus = 0
 local str = string.rep('-', vim.api.nvim_win_get_width(0))
-vim.opt.statusline = '%#VerticalSplit#'..str..'%*'
+vim.o.statusline = '%#VerticalSplit#'..str..'%*'
 -- set cmd height, set by noice.nvim
--- vim.opt.cmdheight = 0
+-- vim.o.cmdheight = 0
 
-vim.opt.title = true
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
-vim.opt.errorbells = false
-vim.opt.mouse = 'a'
-vim.opt.mousemodel = 'extend'
+vim.o.title = true
+vim.o.termguicolors = true
+vim.o.background = "dark"
+vim.o.errorbells = false
+vim.o.mouse = 'a'
+vim.o.mousemodel = 'extend'
 
 -- split window setting
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+vim.o.splitbelow = true
+vim.o.splitright = true
 
 -- hidden conceal char
--- vim.opt.conceallevel = 3
+-- vim.o.conceallevel = 3
 
-vim.opt.modifiable = true
+vim.o.modifiable = true
 -- the refresh time will affect lspsaga's reaction time
-vim.opt.updatetime = 300
+vim.o.updatetime = 300
 -- case settintg
-vim.opt.smartcase = true
-vim.opt.ignorecase = true
-vim.opt.cursorline = true
+vim.o.smartcase = true
+vim.o.ignorecase = true
+vim.o.cursorline = true
 
 -- clipboard
-vim.opt.clipboard = 'unnamedplus'
+vim.o.clipboard = 'unnamedplus'
 
 -- folder
-vim.opt.foldcolumn = '0' -- '0' is not bad
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 99
-vim.opt.foldenable = true
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldcolumn = '0' -- '0' is not bad
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 
-vim.opt.scrolloff = 0
+vim.o.scrolloff = 0
 
 -- indent setting
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+vim.o.autoindent = true
+vim.o.smartindent = true
 
 -- no additional files
-vim.opt.swapfile = false
-vim.opt.backup = false -- insert mode compeleting menu setting
-vim.opt.completeopt = 'menuone,noinsert,noselect'
+vim.o.swapfile = false
+vim.o.backup = false -- insert mode compeleting menu setting
+vim.o.completeopt = 'menuone,noinsert,noselect'
 
 -- visual selection mode
-vim.opt.sel = 'inclusive'
+vim.o.sel = 'inclusive'
 
 -- set tab to 4 spaces, could be override by ftplugin
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
 
 -- show numbers
-vim.opt.number = true
-vim.opt.relativenumber = false
+vim.o.number = true
+vim.o.relativenumber = false
 
 -- save space for window
-vim.opt.signcolumn = 'number'
+vim.o.signcolumn = 'number'
 
 -- wrap line when too long to display
-vim.opt.wrap = true
+vim.o.wrap = true
 
 vim.g.mapleader = ' '
 
-vim.opt.viewoptions = 'folds,cursor,curdir'
+vim.o.viewoptions = 'folds,cursor,curdir'
 
 -- save undo history
-vim.opt.undofile = true
+vim.o.undofile = true
+
 
 vim.g.python3_host_prog = vim.fn.exepath("python3")
 
