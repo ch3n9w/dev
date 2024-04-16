ede79bb0201828086aa40531e4a36595330e450d
bea1e <490584635@qq.com>
2024-04-16 23:34:34 +0800

    add dependency installation for macos

--- scripts/install-software.sh
+++ scripts/install-software.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 
-if [ "$(id -u)" -eq 0 ]; then
-    echo "Current user is root, check passed"
-else
-    echo "switch to root..."
-    if sudo -s true; then
-        echo "switch to root successfully."
+if [ $(uname -s) = "Linux" ]; then
+    if [ "$(id -u)" -eq 0 ]; then
+        echo "Current user is root, check passed"
     else
-        echo "Failed to switch to root."
-        exit 1
+        echo "switch to root..."
+        if sudo -s true; then
+            echo "switch to root successfully."
+        else
+            echo "Failed to switch to root."
+            exit 1
+        fi
+    fi
+
+
+    if [ -f /etc/debian_version ]; then
+    echo "Detect Debian based system, installing packages with apt..."
+    apt update
+    apt install -y git tmux zsh lf ranger kitty rsync htop bat fzf python3 unzip fd-find lsd wget ripgrep neovim zathura todotxt-cli
+    curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+    ln -s /usr/bin/batcat /usr/bin/bat
+    ln -s /usr/bin/fdfind /usr/bin/fd
+    ln -s /usr/bin/python3 /usr/bin/python
+    echo "Please install glow manually if you want, check https://github.com/charmbracelet/glow?tab=readme-ov-file#installation."
+    fi
+
+    if [ -f /etc/arch-release ]; then
+    echo "Detect Arch based system, installing packages with pacman..."
+    pacman -Sy
+    pacman -S --needed --noconfirm git tmux zsh lf ranger kitty rsync htop bat python fzf unzip zoxide lsd fd wget ripgrep neovim glow zathura
+    echo "Please install todotxt-cli from AUR if you want."
     fi
 fi
 
-
-if [ -f /etc/debian_version ]; then
-  echo "Detect Debian based system, installing packages with apt..."
-  apt update
-  apt install -y git tmux zsh lf ranger kitty rsync htop bat fzf python3 unzip fd-find lsd wget ripgrep neovim zathura todotxt-cli
-  curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-  ln -s /usr/bin/batcat /usr/bin/bat
-  ln -s /usr/bin/fdfind /usr/bin/fd
-  ln -s /usr/bin/python3 /usr/bin/python
-  echo "Please install glow manually if you want, check https://github.com/charmbracelet/glow?tab=readme-ov-file#installation."
-fi
-
-if [ -f /etc/arch-release ]; then
-  echo "Detect Arch based system, installing packages with pacman..."
-  pacman -Sy
-  pacman -S --needed --noconfirm git tmux zsh lf ranger kitty rsync htop bat python fzf unzip zoxide lsd fd wget ripgrep neovim glow zathura
-  echo "Please install todotxt-cli from AUR if you want."
+# macOS
+if [ $(uname -s) = "Darwin" ]; then
+  echo "Detect macOS, installing packages with homebrew..."
+  # https://github.com/zegervdv/homebrew-zathura
+  brew tap zegervdv/zathura
+  brew update
+  brew install git tmux zsh lf ranger kitty rsync htop bat python fzf unzip zoxide lsd fd wget ripgrep neovim glow zathura
 fi
 
 git submodule init
