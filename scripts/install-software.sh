e9e7d60fbd6f6098fc23fcaeda86780dc7b47267
qianfei11 <490584635@qq.com>
2024-04-17 20:27:29 +0800

    fix some bugs in scripts/install-sofware.sh

--- scripts/install-software.sh
+++ scripts/install-software.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-if ! sudo -v &> /dev/null; then
-    echo "Failed to execute sudo. Make sure you have sudo privileges."
-    exit 1
+# set -ex
+
+if [ $(uname -s) = "Linux" ]; then
+    if ! sudo -v &> /dev/null; then
+        echo "Failed to execute sudo. Make sure you have sudo privileges."
+        exit 1
+    fi
 fi
 
 if [ -f /etc/debian_version ]; then
-  echo "Detect Debian based system, installing packages with apt..."
-  sudo apt update
-  sudo apt install -y git tmux zsh lf ranger rsync htop bat fzf python3 unzip fd-find lsd wget ripgrep neovim todotxt-cli clang nodejs
-  curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-  sudo ln -s /usr/bin/batcat /usr/bin/bat
-  sudo ln -s /usr/bin/fdfind /usr/bin/fd
-  sudo ln -s /usr/bin/python3 /usr/bin/python
-  echo "Please install glow and kitty manually if you want"
+    echo "Detect Debian based system, installing packages with apt..."
+    sudo apt update
+    if [ $(cat /etc/lsb-release | grep DISTRIB_RELEASE | cut -d "=" -f 2) == "20.04" ]; then
+        sudo apt install -y git tmux zsh ranger rsync htop bat fzf python3 unzip fd-find wget ripgrep neovim todotxt-cli clang nodejs
+        echo "Please install lf and lsd manually if you want"
+    else
+        sudo apt install -y git tmux zsh lf ranger rsync htop bat fzf python3 unzip fd-find lsd wget ripgrep neovim todotxt-cli clang nodejs
+    fi
+    curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+    sudo ln -s -f /usr/bin/batcat /usr/bin/bat
+    sudo ln -s -f /usr/bin/fdfind /usr/bin/fd
+    sudo ln -s -f /usr/bin/python3 /usr/bin/python
+    echo "Please install glow and kitty manually if you want"
 fi
 
 if [ -f /etc/arch-release ]; then
-  echo "Detect Arch based system, installing packages with pacman..."
-  sudo pacman -Sy
-  sudo pacman -S --needed --noconfirm git tmux zsh lf ranger rsync htop bat python fzf unzip zoxide lsd fd wget ripgrep neovim glow clang nodejs
-  echo "Please install todotxt-cli and kitty if you want."
-
+    echo "Detect Arch based system, installing packages with pacman..."
+    sudo pacman -Sy
+    sudo pacman -S --needed --noconfirm git tmux zsh lf ranger rsync htop bat python fzf unzip zoxide lsd fd wget ripgrep neovim glow clang nodejs
+    echo "Please install todotxt-cli and kitty if you want."
 fi
 
 if [ $(uname -s) = "Darwin" ]; then
-  echo "Detect macOS, installing packages with homebrew..."
-  sudo brew tap zegervdv/zathura
-  sudo brew update
-  sudo brew install git tmux zsh lf ranger kitty rsync htop bat python fzf unzip zoxide lsd fd wget ripgrep neovim glow zathura
+    echo "Detect macOS, installing packages with homebrew..."
+    brew tap zegervdv/zathura
+    brew update
+    brew install git tmux zsh lf ranger kitty rsync htop bat python fzf unzip zoxide lsd fd wget ripgrep neovim glow zathura todo-txt clang nodejs
 fi
 
 # git submodule init
@@ -53,12 +61,12 @@ if [ "$response" == "o" ]; then
     cp -r ./nvim $BASEDIR/
 elif [ "$response" == "b" ]; then
     echo "Backup the configuration file..."
-    mv $BASEDIR/tmux $BASEDIR/tmux_bak
-    mv $BASEDIR/zsh $BASEDIR/zsh_bak
-    mv ~/.zshrc ~/.zshrc_bak
-    mv $BASEDIR/lf $BASEDIR/lf_bak
-    mv $BASEDIR/todo $BASEDIR/todo_bak
-    mv $BASEDIR/nvim $BASEDIR/nvim_bak
+    mv $BASEDIR/tmux $BASEDIR/tmux_bak || echo "$BASEDIR/tmux not found. Skip."
+    mv $BASEDIR/zsh $BASEDIR/zsh_bak || echo "$BASEDIR/zsh not found. Skip."
+    mv ~/.zshrc ~/.zshrc_bak || echo "~/.zshrc not found. Skip."
+    mv $BASEDIR/lf $BASEDIR/lf_bak || echo "$BASEDIR/lf not found. Skip."
+    mv $BASEDIR/todo $BASEDIR/todo_bak || echo "$BASEDIR/todo not found. Skip."
+    mv $BASEDIR/nvim $BASEDIR/nvim_bak || echo "$BASEDIR/nvim not found. Skip."
     cp -r ./tmux $BASEDIR/
     cp -r ./zsh/zsh $BASEDIR/
     cp -r ./zsh/zshrc ~/.zshrc
