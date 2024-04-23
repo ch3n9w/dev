eda7e41824cc90bdfb1a33aa072db05bdec998d8
ch3n9w <ch4xer@gmail.com>
2024-04-23 09:19:27 +0800

    adjust fzf keymap for bufferdelete

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -5,10 +5,17 @@ return {
             'nvim-tree/nvim-web-devicons',
         },
         config = function()
+            local actions = require("fzf-lua").actions
             require("fzf-lua").setup({
                 winopts = {
                     preview = {
                         layout = 'vertical'
+                    }
+                },
+                buffers = {
+                    actions = {
+                        ["ctrl-x"] = false,
+                        ["ctrl-d"] = { actions.buf_del, actions.resume },
                     }
                 }
             })
