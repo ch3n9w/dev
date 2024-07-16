add52b84e5c7fd69e1feb473e95b0e8df3b4bdc4
ch3n9w <ch4xer@gmail.com>
2024-07-16 10:14:04 +0800

    add software confs in zsh

---
+++ zsh/zsh/software/kubectl.zsh
@@ -0,0 +1,9 @@
+export PATH=$PATH:${KREW_ROOT:-$HOME/.krew}/bin
+
+# alias k="minikube kubectl --"
+alias k="kubectl"
+alias ka="kubectl apply"
+alias kd="kubectl describe"
+alias kdt="kubectl delete"
+alias ke="kubectl exec"
+alias kg="kubectl get"
---
+++ zsh/zsh/software/tomcat.zsh
@@ -0,0 +1,6 @@
+# for tomcat debug
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
+export JRE_HOME=$JAVA_HOME/jre
+export CATALINA_HOME=/home/ch4ser/work/apache-tomcat-8.5.89
+export CLASSPATH=$HOME:$JAVA_HOME/jre/lib/rt.jar:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib:$CATALINA_HOME/lib
+export PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin
