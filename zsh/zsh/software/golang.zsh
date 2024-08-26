607f3c26fdf6ff2a8132abd9f444c698587ca8ec
ch3n9w <ch4xer@gmail.com>
2024-08-26 15:33:29 +0800

    update golang conf

--- zsh/zsh/software/golang.zsh
+++ zsh/zsh/software/golang.zsh
@@ -2,5 +2,5 @@ export CGO_ENABLED=1
 export GOPATH=$HOME/Projects/go
 export GOPROXY=https://goproxy.cn
 export GO111MODULE=on
-export PATH=$PATH:$GOPATH/bin:$GOROOT/bin:$GOPATH/bin
+export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
 
