d82bf9f4ed2e7616a39a43001932272684be9835
ch3n9w <ch4xer@gmail.com>
2024-10-21 19:56:05 +0800

    update installer

--- scripts/install-software.sh
+++ scripts/install-software.sh
@@ -3,37 +3,37 @@
 # set -ex
 
 if [ $(uname -s) = "Linux" ]; then
-    if ! sudo -v &> /dev/null; then
-        echo "Failed to execute sudo. Make sure you have sudo privileges."
-        exit 1
-    fi
+  if ! sudo -v &>/dev/null; then
+    echo "Failed to execute sudo. Make sure you have sudo privileges."
+    exit 1
+  fi
 fi
 
 if [ -f /etc/debian_version ]; then
-    echo "Detect Debian based system, installing packages with apt..."
-    sudo apt update
-    if [ $(cat /etc/lsb-release | grep DISTRIB_RELEASE | cut -d "=" -f 2) == "20.04" ]; then
-        sudo apt install -y git kitty tmux zsh rsync htop bat fzf python3 unzip fd-find wget ripgrep neovim clang nodejs npm golang
-        echo "Please install lf and lsd manually if you want"
-    else
-        sudo apt install -y git kitty tmux zsh lf rsync htop bat fzf python3 unzip fd-find lsd wget ripgrep neovim clang nodejs npm golang
-    fi
-    curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-    sudo ln -s -f /usr/bin/batcat /usr/bin/bat
-    sudo ln -s -f /usr/bin/fdfind /usr/bin/fd
-    sudo ln -s -f /usr/bin/python3 /usr/bin/python
+  echo "Detect Debian based system, installing packages with apt..."
+  sudo apt update
+  if [ $(cat /etc/lsb-release | grep DISTRIB_RELEASE | cut -d "=" -f 2) == "20.04" ]; then
+    sudo apt install -y git kitty tmux zsh rsync htop bat fzf python3 unzip fd-find wget ripgrep neovim clang nodejs npm golang
+    echo "Please install lf and lsd manually if you want"
+  else
+    sudo apt install -y git kitty tmux zsh lf rsync htop bat fzf python3 unzip fd-find lsd wget ripgrep neovim clang nodejs npm golang
+  fi
+  curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+  sudo ln -s -f /usr/bin/batcat /usr/bin/bat
+  sudo ln -s -f /usr/bin/fdfind /usr/bin/fd
+  sudo ln -s -f /usr/bin/python3 /usr/bin/python
 fi
 
 if [ -f /etc/arch-release ]; then
-    echo "Detect Arch based system, installing packages with pacman..."
-    sudo pacman -Sy
-    sudo pacman -S --needed --noconfirm kitty git tmux zsh lf rsync htop bat python fzf unzip zoxide lsd fd wget ripgrep neovim glow clang nodejs npm go
+  echo "Detect Arch based system, installing packages with pacman..."
+  sudo pacman -Sy
+  sudo pacman -S --needed --noconfirm kitty git tmux zsh lf rsync htop bat python fzf unzip zoxide lsd fd wget ripgrep neovim glow clang nodejs npm go
 fi
 
 if [ $(uname -s) = "Darwin" ]; then
-    echo "Detect macOS, installing packages with homebrew..."
-    brew update
-    brew install git tmux zsh lf kitty rsync htop bat python fzf unzip zoxide lsd fd wget ripgrep neovim glow clang npm nodejs go
+  echo "Detect macOS, installing packages with homebrew..."
+  brew update
+  brew install git tmux zsh lf kitty rsync htop bat python fzf unzip zoxide lsd fd wget ripgrep neovim glow clang npm nodejs go
 fi
 
 # git submodule init
@@ -44,33 +44,37 @@ BASEDIR=~/.config
 read -p "Do you want to overwrite or backup the configuration file? (o/b): " response
 
 if [ "$response" == "o" ]; then
-    echo "Overwriting the configuration file..."
-    yes | rm -r $BASEDIR/tmux
-    rm ~/.zshrc
-    yes | rm -r $BASEDIR/zsh
-    yes | rm -r $BASEDIR/lf
-    yes | rm -r $BASEDIR/nvim
-    cp -r ./tmux $BASEDIR/
-    cp -r ./zsh/zsh $BASEDIR/
-    cp -r ./zsh/zshrc ~/.zshrc
-    cp -r ./lf $BASEDIR/
-    cp -r ./nvim $BASEDIR/
-    cp -r ./kitty $BASEDIR/
+  echo "Overwriting the configuration file..."
+  yes | rm -r $BASEDIR/tmux
+  rm ~/.zshrc
+  yes | rm -r $BASEDIR/zsh
+  yes | rm -r $BASEDIR/lf
+  yes | rm -r $BASEDIR/nvim
+  cp -r ./tmux $BASEDIR/
+  cp -r ./zsh/zsh $BASEDIR/
+  cp -r ./zsh/zshrc ~/.zshrc
+  cp -r ./lf $BASEDIR/
+  cp -r ./nvim $BASEDIR/
+  cp -r ./kitty $BASEDIR/
 elif [ "$response" == "b" ]; then
-    echo "Backup the configuration file..."
-    mv $BASEDIR/tmux $BASEDIR/tmux_bak || echo "$BASEDIR/tmux not found. Skip."
-    mv $BASEDIR/zsh $BASEDIR/zsh_bak || echo "$BASEDIR/zsh not found. Skip."
-    mv ~/.zshrc ~/.zshrc_bak || echo "~/.zshrc not found. Skip."
-    mv $BASEDIR/lf $BASEDIR/lf_bak || echo "$BASEDIR/lf not found. Skip."
-    mv $BASEDIR/nvim $BASEDIR/nvim_bak || echo "$BASEDIR/nvim not found. Skip."
-    mv $BASEDIR/kitty $BASEDIR/kitty_bak || echo "$BASEDIR/kitty not found. Skip."
-    cp -r ./tmux $BASEDIR/
-    cp -r ./zsh/zsh $BASEDIR/
-    cp -r ./zsh/zshrc ~/.zshrc
-    cp -r ./lf $BASEDIR/
-    cp -r ./nvim $BASEDIR/
-    cp -r ./kitty $BASEDIR/
+  echo "Backup the configuration file..."
+  mv $BASEDIR/tmux $BASEDIR/tmux_bak || echo "$BASEDIR/tmux not found. Skip."
+  mv $BASEDIR/zsh $BASEDIR/zsh_bak || echo "$BASEDIR/zsh not found. Skip."
+  mv ~/.zshrc ~/.zshrc_bak || echo "~/.zshrc not found. Skip."
+  mv $BASEDIR/lf $BASEDIR/lf_bak || echo "$BASEDIR/lf not found. Skip."
+  mv $BASEDIR/nvim $BASEDIR/nvim_bak || echo "$BASEDIR/nvim not found. Skip."
+  mv $BASEDIR/kitty $BASEDIR/kitty_bak || echo "$BASEDIR/kitty not found. Skip."
+  cp -r ./tmux $BASEDIR/
+  cp -r ./zsh/zsh $BASEDIR/
+  cp -r ./zsh/zshrc ~/.zshrc
+  cp -r ./lf $BASEDIR/
+  cp -r ./nvim $BASEDIR/
+  cp -r ./kitty $BASEDIR/
 else
-    echo "Invalid response. Please enter 'o' or 'b'."
-    exit 1
+  echo "Invalid response. Please enter 'o' or 'b'."
+  exit 1
 fi
+
+nvim --headless "+Lazy! sync" +qa
+nvim --headless "+MasonToolsInstallSync" +qa
+nvim --headless "+TSUpdateSync" +qa
