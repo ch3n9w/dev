4f4609932ce8f593ad90ad06388488399dd092d5
ch3n9w <ch4xer@gmail.com>
2024-10-24 11:49:38 +0800

    fix installer

--- scripts/install-software.sh
+++ scripts/install-software.sh
@@ -18,9 +18,6 @@ if [ -f /etc/debian_version ]; then
   else
     sudo apt install -y git kitty tmux zsh lf rsync htop bat fzf python3 unzip fd-find lsd wget ripgrep neovim clang nodejs npm golang python3-pip python3-venv
   fi
-  chmod +x install.sh
-  ./install.sh --yes
-  rm ./install.sh
   curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
   sudo ln -s -f /usr/bin/batcat /usr/bin/bat
   sudo ln -s -f /usr/bin/fdfind /usr/bin/fd
