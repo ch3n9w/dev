03fe8083855d29f6b159d723a074b1a713febe96
ch3n9w <ch4xer@gmail.com>
2024-12-26 10:22:04 +0800

    update

--- zsh/zsh/functions.zsh
+++ zsh/zsh/functions.zsh
@@ -6,22 +6,6 @@ redraw-prompt() {
   p10k display -r
 }
 
-# filemanager() {
-#     tmp="$(mktemp)"
-#     lf --last-dir-path="$tmp" "$@"
-#     if [ -f "$tmp" ]; then
-#         dir="$(cat "$tmp")"
-#         rm -f "$tmp"
-#         if [ -d "$dir" ]; then
-#             if [ "$dir" != "$(pwd)" ]; then
-#                 cd "$dir"
-#                 zoxide add "$dir"
-#                 redraw-prompt
-#             fi
-#         fi
-#     fi
-# }
-
 yy() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
 	yazi "$@" --cwd-file="$tmp"
